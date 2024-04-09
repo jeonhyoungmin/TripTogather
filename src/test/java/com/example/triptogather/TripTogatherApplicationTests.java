51521faa811ea565f93e9868e91734a16b78d109
jeonhyoungmin <jhmin7509@gmail.com>
2024-04-10 00:50:31 +0900

    project init

---
+++ src/test/java/com/example/triptogather/TripTogatherApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.triptogather;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TripTogatherApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
